01bea6159877c6e88275900807fa8ae3dcc884dc
Monu <prankum2100@berkeley.edu>
2016-10-14 13:17:14 -0700

    styles, made upload image handled by upload.jsx

--- app/assets/javascripts/react/components/DropZone.jsx
+++ app/assets/javascripts/react/components/DropZone.jsx
@@ -4,55 +4,6 @@ const NEW_PHOTO_URL = '/photo/new'
 
 class DropZone extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      upload: 'before'
-    };
-  }
-
-  onImageDrop(file) {
-    this.setState({ upload: 'uploading' });
-    let upload = request.post(CLOUDINARY_UPLOAD_URL)
-                        .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
-                        .field('file', file);
-
-    upload.end((err, response) => {
-      if (err) {
-        console.error(err);
-      }
-
-      let cloudURL = response.body.secure_url;
-      if (cloudURL !== '') {
-        $.post(NEW_PHOTO_URL, { url: cloudURL }, (data, status) => {
-
-          if (status === "success") {
-            caption = data.captions[0]
-
-            this.setState({ upload: 'finished' });
-          }
-        });
-      }
-    });
-  }
-
-  status() {
-    if (this.state.upload == 'uploading') {
-      return (
-        <div>
-          <p>Uploading...</p>
-        </div>
-      );
-    } else if (this.state.upload == 'finished') {
-      return (
-        <div>
-          <p>Finished!</p>
-        </div>
-      );
-    }
-    return null;
-  }
-
   render() {
     const config = {
       iconFiletypes: ['.jpg', '.png', '.jpeg'],
@@ -66,16 +17,13 @@ class DropZone extends React.Component {
     };
 
     const eventHandlers = {
-      addedfile: this.onImageDrop.bind(this)
+      addedfile: this.props.uploadImage
     }
 
     return (
         <div className="center-items">
           <div className="valign">
-            <br />
             <DropzoneComponent config={config} eventHandlers={eventHandlers} djsConfig={djsConfig} />
-            <br />
-            {this.status()}
           </div>
         </div>
     );
--- app/assets/javascripts/react/components/Upload.jsx
+++ app/assets/javascripts/react/components/Upload.jsx
@@ -1,20 +1,61 @@
-const mapper = {
-  "choose_photo": 0,
-  "edit_captions": 1,
-  "confirm": 2
-};
+const CLOUDINARY_UPLOAD_PRESET = 'yd1pwftm';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/laucity/upload';
+const NEW_PHOTO_URL = '/photo/new'
 
 class Upload extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      activeStep: 0
+      activeStep: 0,
+      uploading: 'before'
     };
   }
 
   componentDidMount() {
     $('.collapsible').collapsible();  
+  }
+
+  onImageDrop(file) {
+    this.setState({ upload: 'uploading' });
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                        .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                        .field('file', file);
+
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+
+      let cloudURL = response.body.secure_url;
+      if (cloudURL !== '') {
+        $.post(NEW_PHOTO_URL, { url: cloudURL }, (data, status) => {
+
+          if (status === "success") {
+            caption = data.captions[0]
+
+            this.setState({ upload: 'finished' });
+          }
+        });
+      }
+    });
+  }
+
+  status() {
+    if (this.state.upload == 'uploading') {
+      return (
+        <div>
+          <p>Uploading...</p>
+        </div>
+      );
+    } else if (this.state.upload == 'finished') {
+      return (
+        <div>
+          <p>Finished!</p>
+        </div>
+      );
+    }
+    return null;
   }
 
   render() {
@@ -27,7 +68,10 @@ class Upload extends React.Component {
           <li id="yo">
             <div id="choose_photo" className="collapsible-header active"><i className="material-icons">filter_drama</i>Choose a Photo</div>
             <div className="collapsible-body">
-              <DropZone />
+              <div className="center-items collapsible-elem">
+                <DropZone uploadImage={this.onImageDrop.bind(this)} />
+                {this.status()}
+              </div>
             </div>
           </li>
           <li>
