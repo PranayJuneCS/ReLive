3f45766ee2364fb08ede81c11236491db4224710
Monu <prankum2100@berkeley.edu>
2016-10-14 14:23:13 -0700

    wip carousel upload

--- app/assets/javascripts/react/components/AppBar.jsx
+++ app/assets/javascripts/react/components/AppBar.jsx
@@ -16,8 +16,8 @@ class AppBar extends React.Component {
               <a href="#" data-activates="slide-out" className="button-collapse"><i className="material-icons">menu</i></a>
               <a href="#" className="brand-logo"><i className="material-icons">filter</i>Filterz {/*<em style={{fontSize: "14px"}}>CalHacks 3.0</em>*/}</a>
 
-              <div className="row">
-                <div className="right col s2">
+              <div className="row hide-on-med-and-down">
+                <div className="right col s3">
                   <ul className="tabs">
                     <li id="#" className="tab col s1"><a id="#" href="#" className="nav-anchor">Gallery</a></li>
                     <li id="#upload" className="tab col s1"><a id="#upload" href="#upload" className="nav-anchor">Upload</a></li>
--- app/assets/javascripts/react/components/Upload.jsx
+++ app/assets/javascripts/react/components/Upload.jsx
@@ -18,19 +18,25 @@ class Upload extends React.Component {
       pic_url: null
     };
 
-    $('body').on('click', '.collapsible-header', (event) => {
-      if (event.target.className.includes("active")) {
-        this.setState({ activeStep: mapper[event.target.id]});
-      } else {
-        this.setState({ activeStep: null })
-      }
-      console.log(this.state.activeStep);
-    });
+    // $('body').on('click', '.collapsible-header', (event) => {
+    //   if (event.target.className.includes("active")) {
+    //     this.setState({ activeStep: mapper[event.target.id]});
+    //   } else {
+    //     this.setState({ activeStep: null })
+    //   }
+    //   console.log(this.state.activeStep);
+    // });
 
   }
 
   componentDidMount() {
-    $('.collapsible').collapsible(); 
+    $('.collapsible').collapsible();
+    $('.carousel.carousel-slider').carousel({
+      full_width: true,
+      no_wrap: true,
+      time_constant: 300,
+      dist: 0
+    });
   }
 
   onImageDrop(file) {
@@ -75,32 +81,47 @@ class Upload extends React.Component {
     return null;
   }
 
+  // <div className="container center-align">
+  //       <h1>Upload a Photo</h1>
+
+  //       <ul className="collapsible" data-collapsible="accordion">
+  //         <li id="yo">
+  //           <div id="choose_photo" className="collapsible-header active"><i className="material-icons">filter_drama</i>Choose a Photo</div>
+  //           <div className="collapsible-body">
+  //             <div className="center-items collapsible-elem">
+  //               <DropZone uploadImage={this.onImageDrop.bind(this)} />
+  //               {this.status()}
+  //             </div>
+  //           </div>
+  //         </li>
+  //         <li>
+  //           <div id="edit_captions" className="collapsible-header"><i className="material-icons">place</i>Edit Caption</div>
+  //           <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+  //         </li>
+  //         <li>
+  //           <div id="confirm" className="collapsible-header"><i className="material-icons">whatshot</i>Confirm</div>
+  //           <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+  //         </li>
+  //       </ul>
+
+
+  //     </div>
+
   render() {
     return (
-      <div className="container center-align">
-        <h1>Upload a Photo</h1>
-
-        <ul className="collapsible" data-collapsible="accordion">
-          <li id="yo">
-            <div id="choose_photo" className="collapsible-header active"><i className="material-icons">filter_drama</i>Choose a Photo</div>
-            <div className="collapsible-body">
-              <div className="center-items collapsible-elem">
-                <DropZone uploadImage={this.onImageDrop.bind(this)} />
-                {this.status()}
-              </div>
-            </div>
-          </li>
-          <li>
-            <div id="edit_captions" className="collapsible-header"><i className="material-icons">place</i>Edit Caption</div>
-            <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-          </li>
-          <li>
-            <div id="confirm" className="collapsible-header"><i className="material-icons">whatshot</i>Confirm</div>
-            <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
-          </li>
-        </ul>
-
-
+      <div className="carousel carousel-slider center custom-carousel" data-indicators="true">
+        <div className="carousel-item red white-text" href="#one!">
+          <h2>Choose a Photo</h2>
+          <p className="white-text">Choose a Photo</p>
+        </div>
+        <div className="carousel-item amber white-text" href="#two!">
+          <h2>Second Panel</h2>
+          <p className="white-text">This is your second panel</p>
+        </div>
+        <div className="carousel-item green white-text" href="#three!">
+          <h2>Third Panel</h2>
+          <p className="white-text">This is your third panel</p>
+        </div>
       </div>
     );
   }
