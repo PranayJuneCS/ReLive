604259c166e3cb1da6c4c56aaa814f5c0db7f5de
Rohit Lalchandani <rohitx15@berkeley.edu>
2016-11-12 14:43:43 -0800

    fuck unirest

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -11,19 +11,34 @@ class Photo < ApplicationRecord
   after_create :process_ai
 
   def process_ai
-    response = Unirest.post "https://api.projectoxford.ai/vision/v1.0/describe",
-                            headers: {'Content-Type': 'application/json', 'Ocp-Apim-Subscription-Key': '68c50c26771e49fe85f6640afd72ba9e'},
-                            parameters: { 'url': self.url }.to_json
+    require 'json'
+    require 'net/http'
 
-    puts response.body
-    if response.body["description"]
-      tags = response.body["description"]["tags"]
+    uri = URI('https://api.projectoxford.ai/vision/v1.0/analyze?visualFeatures=Description&language=en')
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    # Request headers
+    request['Content-Type'] = 'application/json'
+    # Request headers
+    request['Ocp-Apim-Subscription-Key'] = 'f82254a0c2d94549b7363616bbc6743f'
+    # Request body
+    request.body = { 'url': self.url }.to_json
+
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+    end
+
+    body = JSON.parse(response.body)
+    puts body
+
+    if body["description"]
+      tags = body["description"]["tags"]
 
       length = [5, tags.length].min
       length.times do |i|
         Tag.create(text: tags[i], photo_id: self.id)
       end
-      caption = response.body["description"]["captions"]
+      caption = body["description"]["captions"]
       if caption[0]
         Caption.create(text: caption[0]["text"], photo_id: self.id)
       end
@@ -32,23 +47,33 @@ class Photo < ApplicationRecord
     end
 
     # Make a new request for emotions
-    response = Unirest.post "https://api.projectoxford.ai/emotion/v1.0/recognize",
-                            headers: { "Content-Type": "application/json", "Host": "api.projectoxford.ai", "Ocp-Apim-Subscription-Key": "1174d709d44e412dbcc174d62bf2ba54" },
-                            parameters: { 'url': self.url }.to_json
-    require 'json'
-    emotions = Hash.from_xml(response.body)
-    data = emotions["ArrayOfFaceRecognitionResult"]["FaceRecognitionResult"]
+    uri = URI('https://api.projectoxford.ai/emotion/v1.0/recognize')
+    uri.query = URI.encode_www_form({
+    })
+
+    request = Net::HTTP::Post.new(uri.request_uri)
+    # Request headers
+    request['Content-Type'] = 'application/json'
+    # Request headers
+    request['Ocp-Apim-Subscription-Key'] = '1174d709d44e412dbcc174d62bf2ba54'
+    # Request body
+    request.body = { 'url': self.url }.to_json
+
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+    end
+
+    puts response.body
+
+    data = JSON.parse(response.body)
     if data.kind_of?(Array)
       # an array of datapoints
-    else
-      # a single datapoint
-      coordinates = data["faceRectangle"];
-      scores = data["scores"]
+      coordinates = data[0]["faceRectangle"];
+      scores = data[0]["scores"]
       max_score = scores.max_by { |k, v| v.to_f }
       Face.create(height: coordinates["height"], left: coordinates["left"], top: coordinates["top"],
         width: coordinates["width"], emotion: max_score[0], score: max_score[1], photo_id: self.id);
     end
-
 
   end
 
