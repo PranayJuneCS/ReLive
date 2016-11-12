71b024b39cad3b08c8b53918f6eda36caad4fff4
Rohit Lalchandani <rohitx15@berkeley.edu>
2016-11-12 14:05:38 -0800

    fix merge

--- app/assets/javascripts/index.jsx
+++ app/assets/javascripts/index.jsx
@@ -3,16 +3,6 @@ $(() => {
     <App />,
   document.getElementById('app'));
 
-<<<<<<< HEAD
   $('.photoCard').addClass('fadeInRight');
-
-  $.ajaxSetup({
-    headers: {
-      'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-    }
-  });
-=======
-  
->>>>>>> 4e4eed553290fefd57cf714edc5437cbc75d65c1
 
 });
